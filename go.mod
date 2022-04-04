39ccb1a3cc9139d01e8af168c869eda9da5b65ee
agisnur24 <agisnur94@gmail.com>
2022-04-04 21:38:37 +0700

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/agisnur24/go-say-hello
+
+go 1.18
---
+++ say_hello.go
@@ -0,0 +1,9 @@
+package go_say_hello
+
+func SayHello(nama string) string {
+	return "Hello " + nama
+}
+
+func Kali(a int, d int) int {
+	return a * d
+}
